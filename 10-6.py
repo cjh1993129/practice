8607fdb779317d5e911a1dd355fb315a233b0ddc
cjh1993129 <cjh1993129@gmail.com>
2018-09-08 22:28:09 +0800

    2st

--- 10-6.py
+++ 10-6.py
@@ -1,26 +1,14 @@
 import json
-def loadfile():
-	filename='username.json'
+def number():
+	filename='number.json'
 	try:
 		with open(filename) as f:
-			username=json.load(f)
+			number=json.load(f)
 	except FileNotFoundError:
-		return None
+		number=input('your number:')
+		with open(filename,'w') as f:
+			json.dump(number,f)
+		print('stored')
 	else:
-		return username
-
-def buildname():
-	username=input('name:    ')
-	filename = 'username.json'
-	with open(filename,'w') as f:
-		json.dump(username,f)
-	return username
-
-def greet():
-	username=loadfile()
-	if username:
-		print('welcome '+username)
-	else:
-		username=buildname()
-		print('remember you '+username)
-greet()
+		print('favourite number: '+number)
+number()
